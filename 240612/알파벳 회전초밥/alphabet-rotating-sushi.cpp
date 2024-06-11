89ee40e7902d3f39e40219f71e6fa1e90e5a85ca
Blanquette0315 <119724857+Blanquette0315@users.noreply.github.com>
2024-06-12 00:23:34 +0900

    Title: 알파벳 회전초밥, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240612/알파벳 회전초밥/alphabet-rotating-sushi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string A;
+    string B;
+    int answer = 1;
+
+    cin >> A >> B;
+    
+    vector<int> vec_idx(B.length());
+
+    for (size_t i = 0; i < B.length(); ++i)
+    {
+        vec_idx[i] = A.find(B[i]);
+    }
+
+    for (size_t i = 0; i < vec_idx.size() - 1; ++i)
+    {
+        if (vec_idx[i] > vec_idx[i + 1])
+        {
+            answer++;
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
